4437994c4d4d447d777db5a0aacb2a1d39628774
BAsnetSujan420 <basnetsujan1828420@gmail.com>
2022-01-26 20:18:06 +0545

    Basic of Arrays

---
+++ Data Types/Arrays.js
@@ -0,0 +1,24 @@
+let dryFruits = ["Cashew", "Almond", "Pistachio"];
+
+console.log(dryFruits[0]); // Cashew
+console.log(dryFruits[1]); // Almond
+console.log(dryFruits[2]); // Pistachio
+console.log(dryFruits); // Cashew,Almond,Pistachio
+
+let vegetables = ["Capsicum", "Carrot", "Cauliflower"];
+
+let shoppingCart = vegetables;
+
+shoppingCart.push("Spinach");
+
+console.log(fruits.length); // 4
+
+let fruits = ["Apple", "Orange", "Pear"];
+
+fruits.pop();
+
+console.log(fruits); // Apple, Orange
+
+fruits.push("Pear");
+
+console.log(fruits); // Apple, Orange, Pear
